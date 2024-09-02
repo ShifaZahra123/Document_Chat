9841311aba665aff799bd1c4ac45d2182541bfd6
Shifa Zahra <140159309+ShifaZahra123@users.noreply.github.com>
2024-09-02 18:33:32 +0500

    Update Mutiple_pdf.py

--- Mutiple_pdf.py
+++ Mutiple_pdf.py
@@ -5,7 +5,7 @@ import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings # Provide Embeddings by Google by using Google API Keys, Vector Embedding Technique (Convert Chunks of Text to Vectors)
 import google.generativeai as genai
 from langchain.vectorstores import FAISS  # Vector store DB created by Facebook doing Similarity Search
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI  # For chat with documnets
 from langchain.chains.question_answering import load_qa_chain # For chat with documnets
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
@@ -14,23 +14,17 @@ load_dotenv()
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-
-
-
-
-
-def get_pdf_text(pdf_docs):
+def get_pdf_text(pdf_docs):  # Text Extraction From pdf
     text=""
-    for pdf in pdf_docs:
+    for pdf in pdf_docs:  
         pdf_reader= PdfReader(pdf)
-        for page in pdf_reader.pages:
+        for page in pdf_reader.pages:  # Pdf reader is in form of list
             text+= page.extract_text()
     return  text
 
 
-
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+def get_text_chunks(text):   # Text Convert into Smaller chunks of size 10000
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000) # To avid missing texts from pdf and chunk_size is large so chunk_overlap also becomes larger
     chunks = text_splitter.split_text(text)
     return chunks
 
@@ -38,7 +32,7 @@ def get_text_chunks(text):
 def get_vector_store(text_chunks):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    vector_store.save_local("faiss_index")
+    vector_store.save_local("faiss_index") # Storing Database in Local Environment
 
 
 def get_conversational_chain():
@@ -55,14 +49,14 @@ def get_conversational_chain():
     model = ChatGoogleGenerativeAI(model="gemini-pro",
                              temperature=0.3)
 
-    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"]) # langchain function PromptTemplate()
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt) # For internal text summarization: chain_type="stuff"
 
     return chain
 
 
 
-def user_input(user_question):
+def user_input(user_question): # text box functionality
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     
     new_db = FAISS.load_local("faiss_index", embeddings)
@@ -82,8 +76,8 @@ def user_input(user_question):
 
 
 def main():
-    st.set_page_config("Chat PDF")
-    st.header("Chat with PDF using Gemini💁")
+    st.set_page_config("Chat with Multiple PDF")
+    st.header("Chat with PDF💁")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
